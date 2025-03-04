de5dbdd039b6705ae1821db994c6e060b4bbe002
GenjiHD <joaquincastrosarmientosanchez@gmail.com>
2025-03-03 23:23:35 -0700

    salvada en teoria

--- Back-end/src/config/firebase.ts
+++ Back-end/src/config/firebase.ts
@@ -3,11 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Cargar las credenciales del archivo JSON
-const serviceAccount = require("../serviceAccountKey.json"); // Ruta al archivo JSON
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.applicationDefault()
 });
 
 const db = admin.firestore();
